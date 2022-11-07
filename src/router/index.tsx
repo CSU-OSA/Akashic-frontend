ed6324c6e5b9dca50cca6b9e71aa3cf0d364c072
SunshineZerg <8208201214@csu.edu.cn>
2022-11-07 14:44:09 +0800

    feat: add PostPage

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { Home } from "../views/Home";
+import {PostPage} from "../views/PostPage";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,6 +8,10 @@ const router = createRouter({
     {
       path: "/",
       component: <Home/>
+    },
+    {
+      path: "/postpage",
+      component: <postpage/>
     }
   ]
 })
---
+++ src/views/PostPage.tsx
@@ -0,0 +1,3 @@
+export function PostPage() {
+  return <h1 class="flex justify-items-center items-center text-9xl">Akashic</h1>
+}
