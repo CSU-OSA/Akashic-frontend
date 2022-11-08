1796e0d76ee02e7748de7327254d9250df6ff517
rick lin <8208200137@csu.edu.cn>
2022-11-08 16:46:18 +0800

    feat: add dep of casdoor-vue-sdk

---
+++ dep.d.ts
@@ -0,0 +1,4 @@
+declare module "casdoor-vue-sdk" {
+  let instance: any;
+  export default instance;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,26 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import naive from "naive-ui";
+import casdoor from "casdoor-vue-sdk";
 
 import App from "./App.vue";
 import router from "./router";
 
 import "./style/style.css";
 
+const config = {
+  serverUrl: "https://auth.dev.magicalsheep.cn",
+  clientId: "e653e70adca839906ae3",
+  organizationName: "csu-osa",
+  appName: "akashic",
+  redirectPath: "/callback",
+};
+
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
 app.use(naive);
+app.use(casdoor,config);
 
 app.mount("#app");
