051799f54cad35442896270f165ac227a7b5aa43
chen-jm <chenjmcjm@foxmail.com>
2022-11-08 12:32:16 +0800

    HomeView Page init

---
+++ src/components/Home/HomeLayout.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="home">
+    <div class="left">
+      <UserComponent />
+    </div>
+    <div class="middle">
+      Message
+    </div>
+    <div class="right">
+      FootPoint
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import UserComponent from "./UserComponent.vue"
+</script>
+
+<style scoped>
+.home {
+  display: flex;
+  justify-content: space-between;
+}
+
+.left,
+.middle,
+.right {
+  height: 100%;
+}
+
+.left {
+  width: 400px;
+  background: azure;
+}
+
+.right {
+  width: 200px;
+  background: floralwhite;
+}
+
+.middle {
+  background: floralwhite;
+  width: 100%;
+  margin: 0 20px;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <HomeLayout />
+  </div>
+</template>
+
+<script setup lang="ts">
+import HomeLayout from "@/components/Home/HomeLayout.vue";
+</script>
+
+<style scoped>
+
+</style>
