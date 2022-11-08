ca351132c6b55a436ccd9fde00ef956aad0ac99d
Tension <844859315@qq.com>
2022-11-08 16:29:07 +0800

    新建文件上传页

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import { Home } from "../views/Home";
 import Login from "../views/LoginView.vue";
+import Upload from "../views/FileUploadView.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,6 +14,10 @@ const router = createRouter({
       path: "/login",
       component: <Login />,
     },
+    {
+      path: "/upload",
+      component: <Upload />
+    }
   ],
 });
 
---
+++ src/views/FileUploadView.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <n-upload multiple directory-dnd action="https://www.mocky.io/v2/5e4bafc63100007100d8b70f" :max="5">
+        <n-upload-dragger>
+            <div style="margin-bottom: 12px">
+                <n-icon size="48" :depth="3">
+                    <archive-icon />
+                </n-icon>
+            </div>
+            <n-text style="font-size: 16px">
+                点击或者拖动文件到该区域来上传
+            </n-text>
+        </n-upload-dragger>
+    </n-upload>
+    </div>
+    
+
+</template>
+
+<script lang="ts" setup>
+
+import { ArchiveOutline as ArchiveIcon } from '@vicons/ionicons5'
+
+
+
+</script>
