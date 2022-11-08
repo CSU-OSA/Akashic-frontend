a63b38637b44ecd7260fdd8ae0ac9fe870ce3296
chen-jm <chenjmcjm@foxmail.com>
2022-11-08 12:32:16 +0800

    HomeView Page init

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,12 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
-import { Home } from "../views/Home";
+import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
-      component: <Home />,
+      component: HomeView,
     },
   ],
 });
