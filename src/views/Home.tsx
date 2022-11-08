16622580631360fa1db97aa594e877d92cdeb19a
chen-jm <chenjmcjm@foxmail.com>
2022-11-08 12:34:45 +0800

    HomeView Page init

--- src/views/Home.tsx
+++
@@ -1,7 +0,0 @@
-export function Home() {
-  return (
-    <main class="flex justify-center items-center">
-      <h1 class="flex justify-items-center items-center text-9xl">Akashic</h1>
-    </main>
-  );
-}
