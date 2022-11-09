888234a589e8f266f8c21e9ecb813f45b3515681
devRickLin <8208200137@csu.edu.cn>
2022-11-09 08:39:53 +0800

    style: lint files

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import { Home } from "../views/Home";
 import Login from "../views/LoginView.vue";
-import Upload from "../views/FileUploadView.vue"
+import Upload from "../views/FileUploadView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,8 +16,8 @@ const router = createRouter({
     },
     {
       path: "/upload",
-      component: <Upload />
-    }
+      component: <Upload />,
+    },
   ],
 });
 
