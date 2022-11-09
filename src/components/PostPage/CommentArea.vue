48fda1a400e0b19d5ce2da7c29505ddde6d4bb1f
SunshineZerg <8208201214@csu.edu.cn>
2022-11-09 16:23:36 +0800

    feat: PostPage commit

---
+++ src/components/PostPage/CommentArea.vue
@@ -0,0 +1,15 @@
+<template>
+  <div v-for="comment in comments" v-bind:key="comment">
+    <n-card title=title hoverable>
+      {{ comment.content}}
+    </n-card>
+  </div>
+</template>
+
+<script setup lang="ts">
+const comments = [
+  { title: "", author: "wyk", content: "这是评论组件", date: "2022/10/1" },
+];
+</script>
+
+<style scoped></style>
