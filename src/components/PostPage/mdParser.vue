b6fe42f9f010a8dcb6dc26dfd4232b140230143e
SunshineZerg <8208201214@csu.edu.cn>
2022-11-10 20:48:50 +0800

    feat: add md parser to PostPage

--- src/components/PostPage/mdParser.vue
+++ src/components/PostPage/mdParser.vue
@@ -5,12 +5,19 @@
 </template>
 
 <script setup lang="ts">
-
   import showdown from "showdown";
   const converter = new showdown.Converter();
-  const text = '# a test for mdParser' + '\n' + '## b';
+  const text = '# a test for *mdP* arser' + '\n' + '## b' + '\n' + '```js' + '哈哈哈';
 
   const html = converter.makeHtml(text);
   console.log(html);
 </script>
-<style></style>
+<style>
+h1{
+  font-size: 100% !important;
+}
+h2{
+  font-size: 200% !important;
+}
+
+</style>
