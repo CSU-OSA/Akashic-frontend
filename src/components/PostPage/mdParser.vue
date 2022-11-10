49d11f66540bfb75a70ba2b6969ca1f01665b882
SunshineZerg <8208201214@csu.edu.cn>
2022-11-10 21:13:53 +0800

    fix: prevent css bug

--- src/components/PostPage/mdParser.vue
+++ src/components/PostPage/mdParser.vue
@@ -7,19 +7,24 @@
 <script setup lang="ts">
   import showdown from "showdown";
   const converter = new showdown.Converter();
-  const text = '# a test for *mdP* arser' + '\n' + '## b' + '\n' + '```js```' + '哈哈哈';
+  const text = '<www.baidu.com>' + '\n' + '# a test for *mdP* arser' + '\n' + '## b' + '\n' + '```js```' + '哈哈哈';
 
   const html = converter.makeHtml(text);
   console.log(html);
 </script>
 <style>
-h1{
+
+.mdParser a{
+  color: #2440b3 !important;
+  font-size: 18px;
+}
+.mdParser h1{
   font-size: 100% !important;
 }
-h2{
+.mdParser h2{
   font-size: 200% !important;
 }
-code{
+.mdParser code{
   display: block;
   padding: 16px;
   margin: 0 0 24px;
