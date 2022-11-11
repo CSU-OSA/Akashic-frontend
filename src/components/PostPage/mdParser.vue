d38cdf4c958554d9745a645888e526f77dd761f4
SunshineZerg <8208201214@csu.edu.cn>
2022-11-11 14:03:59 +0800

    feat: add blockquote

--- src/components/PostPage/mdParser.vue
+++ src/components/PostPage/mdParser.vue
@@ -7,7 +7,7 @@
 <script setup lang="ts">
   import showdown from "showdown";
   const converter = new showdown.Converter();
-  const text = '<www.baidu.com>' + '\n' + '# a test for *mdP* arser' + '\n' + '## b' + '\n' + '```js```' + '哈哈哈';
+  const text = '+a' + '\n' + '1|2|3' + '\n' +'***' + '\n\n' + '![](https://t7.baidu.com/it/u=3923672523,476572507&fm=218&app=126&size=f242,150&n=0&f=JPEG&fmt=auto?s=EEA4576EA6F0DC73520ADA03020060DD&sec=1668272400&t=20be538c02d25824019fd36ba999bf11)' + '>1' + '\n\n' + '<www.baidu.com>' + '\n' + '# a test for *mdP* arser' + '\n' + '## b' + '\n' + '```js```' + '哈哈哈';
 
   const html = converter.makeHtml(text);
   console.log(html);
@@ -24,7 +24,7 @@
 .mdParser h2{
   font-size: 200% !important;
 }
-.mdParser code{
+.mdParser code,blockquote{
   display: block;
   padding: 16px;
   margin: 0 0 24px;
