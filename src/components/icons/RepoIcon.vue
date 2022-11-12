fd6c866e9f430109ae0a9e49faa99b5f4aad5943
devRickLin <8208200137@csu.edu.cn>
2022-11-12 18:15:31 +0800

    feat: add icon components

---
+++ src/components/icons/RepoIcon.vue
@@ -0,0 +1,9 @@
+<template>
+  <n-icon>
+    <all-inbox-round />
+  </n-icon>
+</template>
+<script lang="ts" setup>
+import { AllInboxRound } from "@vicons/material";
+import { NIcon } from "naive-ui";
+</script>
---
+++ src/components/icons/WikiIcon.vue
@@ -0,0 +1,9 @@
+<template>
+  <n-icon>
+    <wikipedia-w />
+  </n-icon>
+</template>
+<script lang="ts" setup>
+import { WikipediaW } from "@vicons/fa";
+import { NIcon } from "naive-ui";
+</script>
