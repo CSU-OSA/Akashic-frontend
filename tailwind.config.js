497e513704af801b72d6c90033e31822591fa6d8
rick lin <8208200137@csu.edu.cn>
2022-11-14 14:06:02 +0800

    fix: config of tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,40 @@
 const theme_default_light = require("./config/themes/default/index.light.css");
 const theme_default_dark = require("./config/themes/default/index.dark.css");
 
+function generateSafeList(themeName) {
+  const keys = [
+    ...new Set(Object.keys({ ...theme_default_dark, ...theme_default_dark })),
+  ];
+  return keys
+    .map((key) =>
+      [`${themeName}-light-${key}`, `${themeName}-dark-${key}`].map((basic) => [
+        basic,
+        `bg-${basic}`,
+        `text-${basic}`,
+      ])
+    )
+    .flat(2);
+}
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: {
-    content: ["src/**/*.{vue,ts,tsx}", "index.html"],
-    theme: {
-      extend: {
-        colors: {
-          default: {
-            light: {
-              ...theme_default_light,
-            },
-            dark: {
-              ...theme_default_dark,
-            },
+  content: ["src/**/*.{vue,ts,tsx}", "index.html"],
+  theme: {
+    extend: {
+      colors: {
+        default: {
+          light: {
+            ...theme_default_light,
+          },
+          dark: {
+            ...theme_default_dark,
           },
         },
       },
     },
-    plugins: [],
-    options: {
-      safeList: ["bg-default-light-primary", "bg-default-dark-primary"],
-    },
+  },
+  purge: {
+    content: ["src/**/*.{vue,ts,tsx}", "index.html"],
+    safelist: generateSafeList("default"),
   },
 };
